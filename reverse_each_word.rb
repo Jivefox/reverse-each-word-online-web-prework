b9e3e6120357b4596ff9d72f1293048093938ffd
Cliff Jurgensen <jivefox.002@gmail.com>
2018-11-24 22:35:20 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
-reverse_each_word(sentence1)
-  sentence1.split(' ')
-  sentence1.each do |x|
+reverse_each_word(string)
+  string.split(' ')
+  string.each do |x|
   x.reverse!
   sentence1.join
 end
